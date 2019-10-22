671e89c19c442ced6bcaa6e19bec494653dc0b8f
Clinton Reed <CREED0043@my.sullivan.edu>
2019-10-22 18:45:05 -0400

    Added functions to add

---
+++ CSC322Funculator/Add.cs
@@ -0,0 +1,38 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace CSC322Funculator
+{
+    public static class Add
+    {
+        [FunctionName("Add")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# Add function processed a request.");
+
+            string num1Parm = req.Query["num1"];
+            string num2Parm = req.Query["num2"];
+
+            //check 2 parms are passed
+
+            if( num1Parm == null || num1Parm ==null) {
+                return new BadRequestObjectResult("Please pass a num1 and num2 on the query string");
+            }
+
+            int num1 = int.Parse(num1Parm);
+            int num2 = int.Parse(num2Parm);
+            int sum = num1 +num2;
+
+            return (ActionResult)new OkObjectResult($"Sum = {sum}");
+        }
+    }
+}
